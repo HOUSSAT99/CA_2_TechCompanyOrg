5c8358b4eea26be8eb9fce2879f5eb1ffdfab303
HOUSSAT99 <zhoussat@gmail.com>
2025-04-24 14:46:43 +0100

    Added recursive merge sort and dummy name list

--- TechCompanyOrg/src/CA_2/TechCompanyOrg.java
+++ TechCompanyOrg/src/CA_2/TechCompanyOrg.java
@@ -39,7 +39,16 @@ public class TechCompanyOrg {
 
             switch (choice) {
                 case SORT:
-                    System.out.println("Sorting logic will go here.");
+                    String[] names = {
+                        "Zakaria", "Alice", "John", "Emily", "Bob", "Charlie", "Dave", "Eva", "Frank", "Grace",
+                        "Helen", "Isaac", "Jill", "Kyle", "Laura", "Mike", "Nina", "Oscar", "Paul", "Quincy",
+                        "Rachel", "Steve", "Tina", "Uma", "Victor", "Wendy", "Xavier", "Yasmine", "Zane"
+                    };
+                    mergeSort(names, 0, names.length - 1);
+                    System.out.println("\nTop 20 Sorted Names:");
+                    for (int i = 0; i < 20 && i < names.length; i++) {
+                        System.out.println((i + 1) + ". " + names[i]);
+                    }
                     break;
                 case SEARCH:
                     System.out.println("Searching logic will go here.");
@@ -59,6 +68,43 @@ public class TechCompanyOrg {
 
         scanner.close();
     }
+    // Recursive Merge Sort
+public static void mergeSort(String[] array, int left, int right) {
+    if (left < right) {
+        int middle = (left + right) / 2;
+
+        mergeSort(array, left, middle);
+        mergeSort(array, middle + 1, right);
+        merge(array, left, middle, right);
+    }
+}
+
+private static void merge(String[] array, int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    String[] L = new String[n1];
+    String[] R = new String[n2];
+
+    for (int i = 0; i < n1; i++)
+        L[i] = array[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = array[middle + 1 + j];
+
+    int i = 0, j = 0, k = left;
+
+    while (i < n1 && j < n2) {
+        if (L[i].compareToIgnoreCase(R[j]) <= 0) {
+            array[k++] = L[i++];
+        } else {
+            array[k++] = R[j++];
+        }
+    }
+
+    while (i < n1) array[k++] = L[i++];
+    while (j < n2) array[k++] = R[j++];
+}
+
     
     
 }
