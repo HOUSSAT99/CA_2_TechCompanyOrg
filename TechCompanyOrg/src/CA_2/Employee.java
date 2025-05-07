c65d3e022c0d8e54304e17c92adfb64795a755fb
HOUSSAT99 <zhoussat@gmail.com>
2025-05-07 23:48:31 +0100

    Enhanced output of sorted employee list with name, manager type, and department

--- TechCompanyOrg/src/CA_2/Employee.java
+++ TechCompanyOrg/src/CA_2/Employee.java
@@ -1,37 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package CA_2;
 
-/**
- *
- * @author Lenovo
- */
 public class Employee {
     private String name;
-    private int employeeID;
     private DepartmentName department;
     private ManagerType managerType;
 
-    public Employee(String name, int employeeID, DepartmentName department, ManagerType managerType) {
+    public Employee(String name, ManagerType managerType, DepartmentName department) {
         this.name = name;
-        this.employeeID = employeeID;
         this.department = department;
         this.managerType = managerType;
     }
 
-    public String getName() { return name; }
-    public int getEmployeeID() { return employeeID; }
-    public DepartmentName getDepartment() { return department; }
-    public ManagerType getManagerType() { return managerType; }
+    public String getName() {
+        return name;
+    }
+
+    public DepartmentName getDepartment() {
+        return department;
+    }
+
+    public ManagerType getManagerType() {
+        return managerType;
+    }
+
     public int compareByName(String otherName) {
-    return this.name.compareToIgnoreCase(otherName);
+        return this.name.compareToIgnoreCase(otherName);
     }
 
     @Override
-    public String toString() {
-        return employeeID + " | " + name + " | " + department + " | " + managerType;
-    }
-    
+public String toString() {
+    return name + " | Manager Type: " + managerType + " | Department: " + department;
+}
+
 }
